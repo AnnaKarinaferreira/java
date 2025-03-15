5c6718a6fa85d0bfa51e152e992859b140572327
annapacheco <annapacheco203@gmail.com>
2025-03-15 11:38:02 -0300

    first commit

---
+++ primeiro_programa/olamundo.java
@@ -0,0 +1,13 @@
+package primeiro_programa;
+
+public class olamundo {
+    
+    public static void main(String [] args){ /*inicio do nosso programa Java */
+
+        System.out.println("Meu primeiro programa em Java");  /*como se fosse o console.log do javacript */
+
+        System.out.println("a versão do java é:" + System.getProperty("java.version"));
+
+    }
+
+}
---
+++ sintaxe_basica/exemplossintaxe.java
@@ -0,0 +1,34 @@
+package sintaxe_basica;
+
+public class exemplossintaxe {
+    public static void main(String[] args) {
+       
+        //1 impressão de texto
+        System.out.println("Podemos imprimir isto aqui, e números também  " + 45);
+        
+        //2 tipos primitivos
+        // variavel = dados guardados
+        // int - integer- inteiro
+        int idade = 18;
+        int ano =  2025;
+
+        System.out.println("A idade de Karina é " + idade + " anos ");
+        System.out.println("O ano atual é " + ano);
+
+
+        //para os que tem numeros apos a virgula por exemplo
+        double altura = 1.68;
+        double pi = 3.14;
+
+        System.out.println("A altura de Karina é de " + altura);
+        System.out.println("O numero pi é " + pi);
+
+        //textos sao as strings
+        String nome = "ferreira";
+
+        System.out.println("O sobrenome de karina é " + nome);
+
+          
+        
+    }
+}
