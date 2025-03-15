e76ae672f9f9502438ca7398f95d8f926e446fbc
annapacheco <annapacheco203@gmail.com>
2025-03-15 14:14:11 -0300

    up

---
+++ javadev/primeirascondicionais.java
@@ -0,0 +1,19 @@
+package java_dev_aulas;
+
+public class primeirascondicionais {
+
+        public static void main(String[] args) {
+            
+        System.out.println("Olá está é a minha primeira linha de código Java");
+
+            boolean bool = false;
+
+            if (bool) {
+                System.out.println("Verdadeiro");
+            } else {
+                System.out.println("Falso");
+            }
+
+        }
+    
+}
